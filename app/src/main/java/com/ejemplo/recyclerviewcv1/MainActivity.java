378166d982583963c8388bae44386485f2224981
Oscar <oscar101019@gmail.com>
2018-02-25 11:59:51 -0700

    recycler view con CardView version 1.0

---
+++ app/src/main/java/com/ejemplo/recyclerviewcv1/MainActivity.java
@@ -0,0 +1,59 @@
+package com.ejemplo.recyclerviewcv1;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+
+    List<DataProvider> productList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productList = new ArrayList<>();
+
+
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Aquí va la adición de elementos a la lista
+        productList.add(
+                new DataProvider("Titulo1", "Otro1",R.drawable.imagen1)
+        );
+        productList.add(
+                new DataProvider("Titulo2", "Otro2",R.drawable.imagen2)
+        );
+        productList.add(
+                new DataProvider("Titulo3", "Otro3",R.drawable.imagen3)
+        );
+        productList.add(
+                new DataProvider("Titulo4", "Otro4",R.drawable.imagen4)
+        );
+
+        adapter = new ProductAdapter(this,productList);
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
+
+
+}
